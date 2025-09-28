c51764539039e5e8284541a25490622ee11e0339
Adedayoke <adedayoke2006@gmail.com>
2025-09-28 23:38:39 +0100

    SOme error fix

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,12 +1,12 @@
-import datetime
+from datetime import datetime, timedelta
 
 def display_current_datetime():
-    current_date = datetime.datetime.now()
+    current_date = datetime.now()
     print(f"Current date and time: {current_date.strftime('%Y-%m-%d %H:%M:%S')}")
 
 def calculate_future_date(days):
-    current_date = datetime.datetime.now()
-    future_date = current_date + datetime.timedelta(days=days)
+    current_date = datetime.now()
+    future_date = current_date + timedelta(days=days)
     print(f"Future date: {future_date.strftime('%Y-%m-%d')}")
 
 def main():
